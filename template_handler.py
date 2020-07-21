1067c11edc8600cd785c3066dc7c6bfda9edf6e1
lorrli <lli9968@gmail.com>
2020-07-21 19:37:40 -0400

    fix up code

--- template_handler.py
+++ template_handler.py
@@ -1,28 +1,28 @@
 import re
 
+
 class Template:
-    def __init__(self, response_pattern, lookup_type):
-        self.pattern = [r"what is (.*)",
-                        r"what is the definition of (.*)",
-                        r"define (.*)",
-                        r"how do you define (.*)"
-                        ]
+    def __init__(self, pattern, response_pattern, lookup_type):
+        self.pattern = pattern
         self.response_pattern = response_pattern
         self.lookup_type = lookup_type
 
 
 def main():
-    temp1 = Template("what is this", "type")
-    print(temp1.pattern)
-
+    what_is_pattern = [r"what is (.*)",
+                       r"what is the definition of (.*)",
+                       r"define (.*)",
+                       r"how do you define (.*)"]
+    temp1 = Template(what_is_pattern, "what is this", "type")
     base_str = "how do you define blob"
     for pattern in temp1.pattern:
         used_pattern = re.compile(pattern)
         if re.search(used_pattern, base_str):
             print("found: ", re.search(used_pattern, base_str).group())
-            print("YAY")
+            return re.search(used_pattern, base_str).group()
         else:
-            print("noo")
+            print("did not find any matches")
+
 
 if __name__ == "__main__":
     main()
