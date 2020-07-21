96e67fd0f4e52be1e81cf896691c4be67ac2b932
lorrli <lli9968@gmail.com>
2020-07-21 19:28:28 -0400

    first attempt at regex matching

--- template_handler.py
+++ template_handler.py
@@ -1,1 +1,28 @@
-#hi
+import re
+
+class Template:
+    def __init__(self, response_pattern, lookup_type):
+        self.pattern = [r"what is (.*)",
+                        r"what is the definition of (.*)",
+                        r"define (.*)",
+                        r"how do you define (.*)"
+                        ]
+        self.response_pattern = response_pattern
+        self.lookup_type = lookup_type
+
+
+def main():
+    temp1 = Template("what is this", "type")
+    print(temp1.pattern)
+
+    base_str = "how do you define blob"
+    for pattern in temp1.pattern:
+        used_pattern = re.compile(pattern)
+        if re.search(used_pattern, base_str):
+            print("found: ", re.search(used_pattern, base_str).group())
+            print("YAY")
+        else:
+            print("noo")
+
+if __name__ == "__main__":
+    main()
