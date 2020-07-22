936a844ad297ed72eb52242237cbe987f9aa3a78
Lina Ly <lily@nerdwallet.com>
2020-07-22 08:41:14 -0700

    Added capture groups

--- template_handler.py
+++ template_handler.py
@@ -1,28 +1,36 @@
 import re
 
-class Template:
-    def __init__(self, response_pattern, lookup_type):
-        self.pattern = [r"what is (.*)",
+what_is_pattern = [r"what is (.*)",
                         r"what is the definition of (.*)",
                         r"define (.*)",
                         r"how do you define (.*)"
                         ]
+what_is_response_pattern = "The definition of {} is {}"
+what_is_type = "DEFINITION"
+
+class Template:
+    def __init__(self, pattern, response_pattern, lookup_type):
+        self.pattern = pattern
         self.response_pattern = response_pattern
         self.lookup_type = lookup_type
 
-
 def main():
-    temp1 = Template("what is this", "type")
-    print(temp1.pattern)
+    what_is_template = Template(what_is_pattern, what_is_response_pattern, what_is_type)
 
     base_str = "how do you define blob"
-    for pattern in temp1.pattern:
+    capture_group = None 
+    for pattern in what_is_template.pattern:
         used_pattern = re.compile(pattern)
-        if re.search(used_pattern, base_str):
+        match = re.search(used_pattern, base_str)
+        if match:
+            capture_group = match.groups()
             print("found: ", re.search(used_pattern, base_str).group())
-            print("YAY")
         else:
             print("noo")
+    
+    result = what_is_template.response_pattern.format(capture_group[0], capture_group[0])
+    print(result)
+    return result
 
 if __name__ == "__main__":
     main()
