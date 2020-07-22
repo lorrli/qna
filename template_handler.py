03772eddffaf226e25f48b80f70e828b2c4dd4eb
lorrli <lli9968@gmail.com>
2020-07-22 18:55:56 -0400

    stripped strings

--- template_handler.py
+++ template_handler.py
@@ -1,4 +1,4 @@
-import re
+import re, string
 
 what_is_pattern = [r"what is (.*)",
                         r"what is the definition of (.*)",
@@ -17,19 +17,24 @@ class Template:
 def main():
     what_is_template = Template(what_is_pattern, what_is_response_pattern, what_is_type)
 
-    base_str = "how do you define blob"
-    capture_group = None 
+    base_str = "what is an the dog??????????????"
+    stripped_str = re.sub(r'\s+\ban\b|\bthe\b|\band\b|\ba\b\s+', '', base_str)
+    final_str = stripped_str.translate(str.maketrans('', '', string.punctuation))
+
+    capture_group = None
     for pattern in what_is_template.pattern:
         used_pattern = re.compile(pattern)
-        match = re.search(used_pattern, base_str)
+        match = re.search(used_pattern, final_str)
         if match:
             capture_group = match.groups()
-            print("found: ", re.search(used_pattern, base_str).group())
+            print("found: ", re.search(used_pattern, final_str).group())
         else:
             print("did not find any matches")
-    
-    result = what_is_template.response_pattern.format(capture_group[0], capture_group[0])
-    print(result)
+    if capture_group:
+        result = what_is_template.response_pattern.format(capture_group[0], capture_group[0])
+        print(result)
+    else:
+        result = None
     return result
 
 if __name__ == "__main__":
